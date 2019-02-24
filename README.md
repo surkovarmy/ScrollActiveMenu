bdf3d07c2b79056ae89824deeab12dd41b59b9da
Maxim <surkovarmy@yandex.ru>
2019-02-25 00:02:49 +0300

    verion 0.2

--- README.md
+++ README.md
@@ -6,11 +6,52 @@
 ```
 ## Использование
 ```bash
-new MenuList();
+new MenuList({
+     'selector': '.ul_top',
+     'class_active': 'active',
+     'speedanimation' : 700,
+     'offset' : 120
+});
 ```
+### Options
+
+| *Название* | *Тип*  | *По умолчанию* | *Описание* |
+|:-----------|:-------|:---------------|:---------------------------------------------------------------------------|
+| **selector**     | string			 | ul_top         | Имя css класса для кнопки вставляемой перед input - кнопка минус  |
+| **class_active**     | string 		 | active          | Имя css класса для активного пункта меню   |
+| **speedanimation**       | int   			 | 700           | Скорость анимации, при клике на якорную ссылку |
+| **offset**       | int   		   | 120           | задает отступ от координат для элементов (относительно документа). 900 - 120(offset)  |
+
 ## Пример
 ```bash
+<ul class="ul_top">
+     <li><a href="#index">Главная</a></li>
+     <li><a href="#service">Услуги</a></li>
+     <li><a href="#contacts">Контакты</a></li>
+     <li><a href="#about">О нас</a></li>
+</ul>
+```
+```bash
+<div id="index">
+    ....
+</div>
+<div id="service">
+    ...
+</div>
+<div id="contacts">
+    ...
+</div>
+<div id="about">
+    ...
+</div>
+```
+```bash
 $(function() {
-    new MenuList();
+    new MenuList({
+        'selector': '.ul_top',
+        'class_active': 'active',
+        'speedanimation' : 700,
+        'offset' : 120
+    });
 });
 ```
--- ScrollActiveMenu.js
+++ ScrollActiveMenu.js
@@ -1,47 +1,56 @@
-var MenuList = function(setting)
+var ScrollActiveMenu = function(setting)
 {
     var defaultSetting = {
         selector: '.ul_top',
         speedanimation: 700,
         offset: 120,
+        class_active: 'active',
     };
     this.setting = $.extend({}, defaultSetting, setting);
     this.container = $(this.setting.selector);
     this.init();
 };
 
-MenuList.prototype = {
+ScrollActiveMenu.prototype = {
     init: function()
     {
         this.handlers();
     },
 
+    check: function() {
+        if (this.container.length < 1){
+            return false
+        }else {
+            return true;
+        }
+    },
+
     handlers: function()
     {
-        $(window).on("scroll", $.proxy(this.scroll_ext, this));
-        this.container.find('a').on('click', $.proxy(this.onClickMenuElement, this));
+        if (this.check()) {
+            $(window).on("scroll", $.proxy(this.scroll_ext, this));
+            this.container.find('a').on('click', $.proxy(this.onClickMenuElement, this));
+        }else {
+            console.log(this.setting.selector + ' - селектор не найден');
+        }
     },
 
     onClickMenuElement: function (event) {
         event.preventDefault();
         var id  = $(event.currentTarget).attr('href');
-        var top = $(id).offset().top-120;
+        var top = $(id).offset().top - this.setting.offset;
         $('body,html').animate({scrollTop: top}, this.setting.speedanimation);
     },
 
-    scroll_ext: function( top ) {
+    scroll_ext: function() {
         var top = $(window).scrollTop();
-        this.container.find('a').removeClass('active');
-        this.container.children('li').each(function(i,elem) {
+        this.container.find('a').removeClass(this.setting.class_active);
+        this.container.children('li').each($.proxy(function(i,elem) {
             var s = $(elem).find('a').attr('href');
-            if ( top <= $(s).offset().top-120 ) {
-                //console.log(this);
-                //console.log(this.container);
-                $(elem).find('a').addClass('active');
+            if ( top <= $(s).offset().top - this.setting.offset ) {
+                $(elem).find('a').addClass(this.setting.class_active);
                 return false;
-            }else {
-                //console.log($(s).top-100);
             }
-        });
+        }, this));
     }
 };
